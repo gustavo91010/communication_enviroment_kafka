b52515d6fd238c8ae48600619103d341c9865f36
gustavo.paes <gustavo910@gmail.com>
2025-01-02 22:44:14 -0300

    feat: start logistc service in kotlin

---
+++ LogisticaService/logistocaServer/src/main/kotlin/com/ajudaqui/logistocaServer/LogistocaServerApplication.kt
@@ -0,0 +1,11 @@
+package com.ajudaqui.logistocaServer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LogistocaServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<LogistocaServerApplication>(*args)
+}
---
+++ LogisticaService/logistocaServer/src/test/kotlin/com/ajudaqui/logistocaServer/LogistocaServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ajudaqui.logistocaServer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LogistocaServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
