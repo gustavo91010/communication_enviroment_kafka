ea8e62c570f8d774616e9f520a20b1acd1473b4f
gustavo.paes <gustavo910@gmail.com>
2025-01-20 09:11:19 -0300

    refactor: work in progess consumer update financial

--- FinanceiroService/services/producer.py
+++ FinanceiroService/services/producer.py
@@ -1,16 +1,49 @@
-import json
 from confluent_kafka import Producer, KafkaException
+from dotenv import load_dotenv
 from utils.avro_services import serialize_avro
+from avro.io import BinaryEncoder, DatumWriter
+from avro.schema import parse
+import json
 
 
-def produce_message(topic, key, value, schema, bootstrap_servers):
+def load_schema(schema_path):
+    with open(schema_path, 'r') as f:
+        schema = json.load(f)
+    return schema
+
+def produce_message(topic, key, value, schema_path, bootstrap_servers):
+    load_dotenv()
+    print(f"\nProduzer mensagem para o topico: {topic}")
+    print(f"\nessa é a mensagem:: {value}")
+    print(f"\nessa é o schema_path:: {schema_path}")
+    if not validate_messgae_with_schema(value,schema_path):
+        print("Mensagem fora do padrão do schema")
+        print(f"Tópico: {topic}, schema_path: {schema_path}")
+        print(json.dumps(value, indent=4))
+        return
+
+    schema = load_schema(schema_path)
     producer = Producer({"bootstrap.servers": bootstrap_servers})
     avro_data = serialize_avro(value, schema)
 
     try:
         producer.produce(topic, key=key, value=avro_data)
         producer.flush()
-        print(f"\nAtualização do financeiro para a solicitação code: {value['code']}\n")
+        print(f"\nMensagem produzida pelo tópico: {topic}\n")
         print(json.dumps(value, indent=4))
     except KafkaException as e:
         print(f"Erro ao produzir mensagem: {e}")
+
+
+def validate_messgae_with_schema(message, schema_path):
+    with open(schema_path, 'r') as f:
+        schema = parse(json.dumps(json.load(f)))
+    try:
+        writer = DatumWriter(schema)
+        bytes_writer = BytesIO()
+        encoder = BinaryEncoder(bytes_writer)
+        writer.write(message, encoder)
+        return True
+    except Exception as e:
+        print(f"Erro de validação na mensagem: {e}")
+        return False
