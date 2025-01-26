3d4627fb863b9a805083321aedf8675b0ebb4cfb
gustavo.paes <gustavo910@gmail.com>
2025-01-26 19:18:15 -0300

    refactor: conage project on python for java

---
+++ FornecedorService/src/test/java/com/ajudaqui/FornecedorService/FornecedorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajudaqui.FornecedorService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FornecedorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
