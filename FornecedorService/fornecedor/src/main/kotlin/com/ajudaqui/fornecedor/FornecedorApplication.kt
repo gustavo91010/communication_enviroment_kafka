ced62f2547d583b11b344c358dc35c46fb901e1f
Gustavo Paes <gustavo.silva@di2win.com>
2025-03-26 22:09:42 -0300

    feat: resove fix index delete row

---
+++ FornecedorService/fornecedor/src/main/kotlin/com/ajudaqui/fornecedor/FornecedorApplication.kt
@@ -0,0 +1,11 @@
+package com.ajudaqui.fornecedor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FornecedorApplication
+
+fun main(args: Array<String>) {
+	runApplication<FornecedorApplication>(*args)
+}
