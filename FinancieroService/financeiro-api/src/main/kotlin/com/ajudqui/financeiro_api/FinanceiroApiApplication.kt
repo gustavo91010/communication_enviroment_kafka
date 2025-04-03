d42fd8f854423fb40e7494acc066793eca85864f
Gustavo Paes <gustavo.silva@di2win.com>
2025-04-02 23:10:56 -0300

    refactor: change financial api by kotlin and include budget request and update avsc

---
+++ FinancieroService/financeiro-api/src/main/kotlin/com/ajudqui/financeiro_api/FinanceiroApiApplication.kt
@@ -0,0 +1,11 @@
+package com.ajudqui.financeiro_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinanceiroApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinanceiroApiApplication>(*args)
+}
---
+++ FinancieroService/financeiro-api/src/test/kotlin/com/ajudqui/financeiro_api/FinanceiroApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ajudqui.financeiro_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinanceiroApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
